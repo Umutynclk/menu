71a27137eae1b70e6c85de7e27eeb8801b35c461
Umut Beytullah Yoncalık <150150072+Umutynclk@users.noreply.github.com>
2026-01-05 16:02:28 +0300

    script.js

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ const categoryImages = {
   "Soğuk İçecekler": "images/soguk_icecekler.jpg",
   "Sıcak İçecekler": "images/sicak_icecekler.jpeg",
   "MilkShake": "images/milkshake.jpg",
-  "Çay&Kahve": "images/Kahve.jpg",
+  "Çay&Kahve": "images/kahve.jpg",
   "Mix&Kokteyl": "images/koktyl.jpg",
   "Bitki Çayı": "images/bitki_cayi.jpeg"
+ "Serinleten Lezzetler": "images/bitki_cayi.jpeg"
 };
 
 const categoriesContainer = document.getElementById("categories");
